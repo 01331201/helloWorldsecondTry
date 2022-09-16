bf4e58f4ad11c9f12be723fa4cab57a400bb71b8
Armend Rexhepi <01331201@edu.campus02.at>
2022-09-16 19:12:46 +0200

    bla bla

---
+++ src/helloWorld.java
@@ -0,0 +1,24 @@
+import java.util.Objects;
+
+public class helloWorld {
+
+    public static void getName(String firstLeter) {
+        if (Objects.equals(firstLeter, "A")) {
+            System.out.println("America");
+        } else {
+            System.out.println("bla bla blas");
+
+        }
+    }
+
+    //main
+    public static void main(String[] args) {
+        getName("E");
+        getName("E");
+        getName("E");
+        getName("a");
+        getName("A");
+        getName("t");
+    }
+
+}
